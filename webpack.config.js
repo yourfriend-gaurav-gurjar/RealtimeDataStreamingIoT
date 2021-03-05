39bd38f2b776555ce0fa871eb80955c466b7ff77
Gaurav Gurjar <55551769+yourfriend-gaurav-gurjar@users.noreply.github.com>
2021-03-05 09:02:43 -0800

    Initial commit - dashboard with flask backend

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "less-loader" //compiles Less to CSS
+                }]
+            }
+        ]
+    },
+    devtool:'cheap-module-source-map'
+}
